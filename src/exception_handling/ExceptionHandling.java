d788eaa3c906e4cba1e35f753e32094b0a27f137
Hakan CERAN <hakanceran64@gmail.com>
2022-09-24 19:12:08 +0300

    Try Catch Finally

---
+++ src/exception_handling/ExceptionHandling.java
@@ -0,0 +1,28 @@
+/*
+ * @File: ArrayListClass.java
+ * @Authors: Hakan CERAN, Engin DEMİROĞ
+ * @Date: 23.09.2022
+ * @Content:
+ * @Notes:
+ */
+
+package exception_handling;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+        int[] sayilar = new int[]{1, 2, 3, 4};
+
+        try {
+            System.out.println(sayilar[10]);
+        } catch (Exception exception) {
+            System.out.println("Hata olustu.");
+            System.out.println(exception);
+        } finally {
+            // finally her zaman çalışır.
+            System.out.println("Bazı durumlarda istem başarılı veya başarısız da olsa sistemi kapatmak gerekebilir.");
+        }
+
+    }
+
+}
